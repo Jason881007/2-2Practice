fb496b066de1be0f2fd11371d99ffc6776f9b43a
Jason881007 <74764436+Jason881007@users.noreply.github.com>
2021-03-30 01:22:05 +0800

    Update Default.aspx.cs

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -9,6 +9,33 @@ namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
 
         protected void Page_Load(object sender, EventArgs e) {
+            int num = 1000;
+            bool a = false;
+
+            if (num % 4 == 0)
+            {
+                a = true;
+
+                if (num % 100 == 0)
+                {
+                    if (num % 400 == 0)
+                    {
+                        a = true;
+                    }
+                    else
+                    {
+                        a = false;
+                    }
+                }
+            }
+            if (a == true)
+            {
+                Response.Write("Y");
+            }
+            else
+            {
+                Response.Write("N");
+            }
         }
     }
 }
